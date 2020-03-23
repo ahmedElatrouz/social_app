e231d314f8ecd251217a9565265a91a070293656
ahmed elatrouz <aelatrouz@gmail.com>
2020-03-23 23:55:13 +0100

    started login page

---
+++ lib/view/login_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class login_page extends StatefulWidget {
+  @override
+  _login_pageState createState() => _login_pageState();
+}
+
+class _login_pageState extends State<login_page> {
+  bool isAuth = false;
+
+  Widget buildAuthScreen() {
+    return Text('authentifié');
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Theme.of(context).accentColor,
+              Theme.of(context).primaryColor,
+            ],
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Social App1',
+              style: TextStyle(
+                  fontFamily: 'signatra',
+                  fontSize: 40.0,
+                  color: Colors.white
+                  ),
+            ),
+            GestureDetector(
+              onTap: ()=>print('clicked'),
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/google_signin_button.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+}
