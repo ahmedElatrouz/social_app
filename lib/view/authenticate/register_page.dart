30ad47f06736ee24cda859162d13dbed13f10f0d
ahmed elatrouz <aelatrouz@gmail.com>
2020-03-31 02:03:21 +0100

    optimiser le code dans register_page

--- lib/view/authenticate/register_page.dart
+++ lib/view/authenticate/register_page.dart
@@ -11,7 +11,6 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
 
@@ -27,159 +26,128 @@ class _RegisterState extends State<Register> {
   String age = '';
   bool loading = false;
 
+  void assignAttribut(String att, var val) {
+    switch (att) {
+      case 'Nom':
+        nom = val;
+        break;
+      case 'Prenom':
+        prenom = val;
+        break;
+      case 'Genre':
+        genre = val;
+        break;
+      case 'Nationalite':
+        nationalite = val;
+        break;
+      case 'Tel':
+        tel = val;
+        break;
+      case 'Email':
+        email = val;
+        break;
+      case 'Password':
+        password = val;
+        break;
+      case 'Age':
+        age = val;
+        break;
+      default:
+        return null;
+        break;
+    }
+  }
+
+  Widget createField(String fieldName) {
+    return Expanded(
+        child: TextFormField(
+          //si on est dans l'email field on change le clavier vers le type email
+      keyboardType: fieldName == "Email"? TextInputType.emailAddress :TextInputType.text,
+      //si on est dans le field password l'ecriture devient obscure (pour que personne ne voit le password)
+      obscureText: fieldName == "Password",
+      validator: (val) => val.isEmpty ? 'cant be empty' : null,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: fieldName,
+          labelStyle: TextStyle(fontSize: 15)),
+      onChanged: (val) {
+        setState(() => assignAttribut(fieldName, val));
+        print(prenom);
+      },
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-
-      appBar: AppBar(
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () => widget.togleView(),
-            icon:Icon(Icons.person),
-            label: Text('SignIn'),
-          )
-        ],
-        backgroundColor: Colors.brown[500],
-        title: Text('SignUp'),
-      ),
-
-
-
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Form(
-            key: _formKey,
-              child: Column(
-        children: <Widget>[
-
-          SizedBox(height: 8,),
-          Expanded(child: TextFormField(
-            validator: (val) => val.isEmpty ? 'can\'t be empty' : null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "nom",
-              labelStyle: TextStyle(fontSize: 15)
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: () => widget.togleView(),
+                  icon: Icon(Icons.person),
+                  label: Text('SignIn'),
+                )
+              ],
+              backgroundColor: Colors.brown[500],
+              title: Text('SignUp'),
             ),
+            body: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 8,
+                        ),
 
-            onChanged: (val) {
-              setState(() => nom = val);
-            },
-          )),
+                        createField("Nom"),
+                        createField("Prenom"),
+                        createField("Genre"),
+                        createField("Nationalite"),
+                        createField("Tel"),
+                        createField("Email"),
+                        createField("Password"),
+                        createField("Age"),
 
-
-          Expanded(child: TextFormField(
-            validator: (val) => val.isEmpty ? 'cant be empty' : null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Prenom",
-              labelStyle: TextStyle(fontSize: 15)
-            ),
-            onChanged: (val) {
-              setState(() => prenom = val);
-            },
-          )),
-
-
-          Expanded(child: TextFormField(
-            validator: (val) => val.isEmpty ? 'cant be empty' : null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "genre",
-              labelStyle: TextStyle(fontSize: 15)
-            ),
-            onChanged: (val) {
-              setState(() => genre = val);
-            },
-          )),
-
-
-          Expanded(child: TextFormField(
-            validator: (val) => val.isEmpty ? 'cant be empty' : null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "nationalite",
-              labelStyle: TextStyle(fontSize: 15)
-            ),
-            onChanged: (val) {
-              setState(() => nationalite = val);
-            },
-          )),
-
-
-          Expanded(child: TextFormField(
-            validator: (val) => val.length==10  ? 'numero de telephone n\'est pas valide' : null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Tel",
-              labelStyle: TextStyle(fontSize: 15)
-            ),
-            onChanged: (val) {
-              setState(() => tel = val);
-            },
-          )),
-
-
-          Expanded(child: TextFormField(
-            validator: (val) => val.isEmpty ? 'cant be empty' : null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Email",
-              labelStyle: TextStyle(fontSize: 15)
-            ),
-            onChanged: (val) {setState(() => email = val);},
-          )),
-
-
-          Expanded(child: TextFormField(
-            validator: (val) => val.isEmpty ? 'cant be empty' : null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Password",
-              labelStyle: TextStyle(fontSize: 15)
-            ),
-            onChanged: (val) {
-              setState(() => password = val);
-            },
-          )),
-
-
-          Expanded(child: TextFormField(
-            validator: (val) => val.isEmpty ? 'cant be empty' : null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Age",
-              labelStyle: TextStyle(fontSize: 15)
-            ),
-            onChanged: (val) {
-              setState(() => age = val);
-            },
-          )),
-
-
-          Expanded(child: RaisedButton(
-            
-            highlightColor: Colors.purple,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30) ),
-            color: Colors.blueAccent,
-            child: Text('Register',style:TextStyle(fontSize: 15.0,color: Colors.white)),
-
-            onPressed: () async{
-              if(_formKey.currentState.validate()){
-                setState(() => loading = true);
-                dynamic result=_auth.signUp(email, password)
-                .then((currentUser) =>_auth.createCollection(age, email, genre, nationalite, nom, password, prenom, tel));
-                if(result == null){
-                  loading = false;
-                  error = 'you have problem in your email';
-                }
-              }
-            },
-          )),
-          SizedBox(height: 8,),
-          //Text(error,style: TextStyle(color: Colors.red),),
-        ],
-      ))),
-    );
+                        Expanded(
+                            child: RaisedButton(
+                          highlightColor: Colors.purple,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          color: Colors.blueAccent,
+                          child: Text('Register',
+                              style: TextStyle(
+                                  fontSize: 15.0, color: Colors.white)),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => loading = true);
+                              dynamic result = _auth
+                                  .signUp(email, password)
+                                  .then((currentUser) => _auth.createCollection(
+                                      age,
+                                      email,
+                                      genre,
+                                      nationalite,
+                                      nom,
+                                      password,
+                                      prenom,
+                                      tel));
+                              if (result == null) {
+                                loading = false;
+                                error = "this email adresse doesn't exist!";
+                              }
+                            }
+                          },
+                        )),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        //Text(error,style: TextStyle(color: Colors.red),),
+                      ],
+                    ))),
+          );
   }
 }
