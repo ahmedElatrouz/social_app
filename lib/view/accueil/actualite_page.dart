db2cc051ab3511a9b6d74e335c77cb4915d87e4e
ahmed elatrouz <aelatrouz@gmail.com>
2020-04-27 15:50:06 +0000

    starting actualite page

--- lib/view/accueil/actualite_page.dart
+++ lib/view/accueil/actualite_page.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:social_app/view/shared/reusable_header.dart';
 
 
 class ActualitePage extends StatefulWidget {
@@ -19,9 +20,24 @@ class _ActualitePageState extends State<ActualitePage> {
     return  //Center(child: Text("actualite"));
     
      Scaffold(
-      //appBar:header(context,"actualite"),
-      backgroundColor: Colors.white,
-      body: Column(
+      appBar: header(context,"actualite"),
+      backgroundColor: Color(0xFFFFFFFF),
+      body:ListView(
+        children: <Widget>[
+          SizedBox(
+              height:10,
+            ),
+          postWidget(),
+          SizedBox(
+              height:10,
+            ),
+          postWidget(),
+          SizedBox(
+              height:10,
+            ),
+          postWidget(),
+        ],
+      ) /*Column(
         children: <Widget>[
           Row(
             children: <Widget>[
@@ -52,7 +68,82 @@ class _ActualitePageState extends State<ActualitePage> {
             ),
           ),
         ],
-      ),
+      ),*/
     );
   }
+
+
+
+
+
+
+
+  Widget postWidget(){
+    return
+      Container(
+        margin: EdgeInsets.symmetric(horizontal:20),
+        decoration: BoxDecoration(
+          color: Color(0xFFF0F0F0),
+          border:Border.all(color: Colors.blueGrey,width: 1 ),
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
+        child: Padding(
+        padding: EdgeInsets.all(20),
+        child: InkWell(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(
+                    "assets/images/ahmed.jpg",
+                  ),
+                ),
+
+                contentPadding: EdgeInsets.all(0),
+                title: Text(
+                  "ahmed lbastos",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                trailing: Text(
+                  "marrakech,lbit diali",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 11,
+                  ),
+                ),
+              ),
+
+              Container(
+                foregroundDecoration: BoxDecoration(
+                borderRadius:BorderRadius.all(Radius.circular(30)),
+                ),
+                decoration: BoxDecoration(
+                  
+                ),
+                child: Image.asset(
+                  "assets/images/ahmed.jpg",
+                  height: 170,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            
+            ],
+          ),
+          onTap: (){},
+        ),
+    ),
+    
+      );
+    
+  }
+
+
+
+
+
+
+
 }
