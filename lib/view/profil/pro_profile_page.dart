45b5bd28f25dd8498c0255bf99158e5e6e71e424
ahmed elatrouz <aelatrouz@gmail.com>
2020-05-01 16:14:35 +0000

    desing professionnal profile page

--- lib/view/profil/pro_profile_page.dart
+++ lib/view/profil/pro_profile_page.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 
-
-
-
-
-
-
-
 class ProProfilPage extends StatefulWidget {
   @override
   _ProProfilPageState createState() => _ProProfilPageState();
 }
 
 class _ProProfilPageState extends State<ProProfilPage> {
-@override
+  @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -28,101 +21,151 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  ScrollController controller;
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        body:  Stack(
-      children: <Widget>[
-        ClipPath(
-          child: Container(color: Colors.black.withOpacity(0.8)),
-          clipper: getClipper(),
-        ),
-        Positioned(
-            width: 320.0,
-            top: MediaQuery.of(context).size.height / 9,
-            child: Column(
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: ListView(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image:DecorationImage(image: AssetImage("assets/images/back.jpg"),fit:BoxFit.cover),
+              ),
+              height: MediaQuery.of(context).size.height / 4,
+              child: Stack(children: <Widget>[
+               /* ClipPath(
+                  child: Container(color: Colors.black.withOpacity(0.8)),
+                  clipper: GetClipper(),
+                ),*/
+                Positioned(
+                  width: 320.0,
+                  top: MediaQuery.of(context).size.height / 13,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                          width: 70.0,
+                          height: 70.0,
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/3.jpg'),
+                                  fit: BoxFit.cover),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(35.0)),
+                              boxShadow: [
+                                BoxShadow(blurRadius: 7.0, color: Colors.black)
+                              ])),
+                    ],
+                  ),
+                )
+              ]),
+            ),
+
+            Column(
+              
               children: <Widget>[
-                Container(
-                    width: 120.0,
-                    height: 120.0,
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        image: DecorationImage(
-                            image: AssetImage(
-                                'assets/images/3.jpg'),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.all(Radius.circular(60.0)),
-                        boxShadow: [
-                          BoxShadow(blurRadius: 7.0, color: Colors.black)
-                        ])),
-                SizedBox(height: 50.0),
+                SizedBox(height: 10.0),
+
+
                 Text(
-                  'Ahme elatrouz',
+                  'Ahmed elatrouz',
                   style: TextStyle(
-                      fontSize: 30.0,
+                      fontSize: 25.0,
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Montserrat'),
                 ),
-                SizedBox(height: 15.0),
+
+
+                SizedBox(height: 7.0),
+
+
+
                 Text(
-                  'Moroccoman',
+                  'Morocco',
                   style: TextStyle(
-                      fontSize: 17.0,
+                      fontSize: 15.0,
                       fontStyle: FontStyle.italic,
                       fontFamily: 'Montserrat'),
                 ),
-                SizedBox(height: 25.0),
+
+
+                SizedBox(height: 5.0),
+
+
                 Container(
-                    height: 30.0,
-                    width: 95.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.greenAccent,
-                      color: Colors.green,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'Edit Name',
-                            style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
-                          ),
-                        ),
-                      ),
-                    )),
-                    SizedBox(height: 25.0),
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  //height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'my name is ahmed and I am watching better call saul, my best serie is breaking bad, '
+                    'I don\'t like game of thrones that much. Favorite anime is hunter x hunter + attack on titan.',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+
+
+                SizedBox(height: 10.0),
+                
+
                 Container(
-                    height: 30.0,
-                    width: 95.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.redAccent,
-                      color: Colors.red,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'Log out',
-                            style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
-                          ),
-                        ),
-                      ),
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                   padding: EdgeInsets.symmetric(horizontal: 20),
+                    // height: 1000,
+                    width: MediaQuery.of(context).size.width - 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  child: Text(
+                    'Categorie: Music',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontWeight:FontWeight.w500,
+                      fontFamily: 'Signatra',
+                      fontSize: 30
+                    ),),
+                  ),
+
+
+                  SizedBox(height: 10.0),
+                
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    // height: 1000,
+                    width: MediaQuery.of(context).size.width - 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        for (int i = 0; i < 200; i++) Text('hello'),
+                      ],
                     ))
               ],
-            ))
-      ],
-    ));
+            )
+          ],
+        ));
   }
 }
 
-class getClipper extends CustomClipper<Path> {
+class GetClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = new Path();
 
-    path.lineTo(0.0, size.height / 3);
-    path.lineTo(size.width + 125, 0.0);
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width + 125, size.height);
+    path.lineTo(size.width, 0.0);
     path.close();
     return path;
   }
